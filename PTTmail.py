00b3f5ae0633ddeeaba96653ee4809e93c1702ed
Unknown <sam841009@yahoo.com.tw>
2018-07-15 23:39:02 +0800

    escape

--- PTTmail.py
+++ PTTmail.py
@@ -20,9 +20,9 @@ class APP:
         self.recipient = ''
         self.title = ''
         self.content = ''
-        
+        self.esc = ''
         win.title("Sent Mail")
-        win.geometry('350x400')
+        win.geometry('250x220')
         self.IDlabel=tk.Label(win, text="ID:")   #建立標籤物件
         self.PWlabel=tk.Label(win, text="Password:")   #建立標籤物件
         self.relabel=tk.Label(win, text="收件人:")   #建立標籤物件
@@ -47,12 +47,12 @@ class APP:
         self.password_input.grid(column=1,row=1,columnspan=3)
         self.relabel.grid(column=0,row=2)
         self.re_input.grid(column=1,row=2,columnspan=3)
-        #self.esclabel.grid(column=0,row=3)
-        #self.esc_input.grid(column=1,row=3,columnspan=3)
-        self.titlelabel.grid(column=0,row=3)
-        self.title_input.grid(column=1,row=3,columnspan=3)
-        self.contentlabel.grid(column=0,row=4)
-        self.content_input.grid(column=1,row=4,columnspan=3,rowspan=5)
+        self.esclabel.grid(column=0,row=3)
+        self.esc_input.grid(column=1,row=3,columnspan=3)
+        self.titlelabel.grid(column=0,row=4)
+        self.title_input.grid(column=1,row=4,columnspan=3)
+        self.contentlabel.grid(column=0,row=5)
+        self.content_input.grid(column=1,row=5,columnspan=3,rowspan=5)
         # r=-1
         # for i in range(20):
         #     if i%2 == 0:c=1
@@ -67,14 +67,23 @@ class APP:
         self.recipient = self.re_input.get("1.0",'end-1c')
         self.title = self.title_input.get("1.0",'end-1c')
         self.content = self.content_input.get("1.0",'end-1c')
+        self.esc = self.esc_input.get("1.0",'end-1c')
         # print(self.recipient.split('@'))
         self.PTTlogin()
     def PTTlogin(self):
-        self.PTTBot = PTT.Library(self.ID, self.Password, kickOtherLogin=False, _LogLevel=PTT.LogLevel.DEBUG)
-        ErrCode = self.PTTBot.login()
+        if self.PTTBot == '':
+            self.PTTBot = PTT.Library(self.ID, self.Password, kickOtherLogin=False, _LogLevel=PTT.LogLevel.DEBUG)
+            ErrCode = self.PTTBot.login()
+        esc_content = self.esc.split('@')
+        i = 0
+        # print(esc_content)
+        escape_content = ''
         for id_num in self.recipient.split('@'):
-            self.SendMail(id_num,self.title,self.content)
-        self.PTTBot.logout()
+            if i < len(esc_content): escape_content = self.content.replace('[指令]', esc_content[i])
+            else :escape_content = self.content
+            i +=1
+            self.SendMail(id_num,self.title,escape_content)
+        #self.PTTBot.logout()
     def SendMail(self,id, title, content):
         ErrCode = self.PTTBot.mail(id, title, content, 0)
         if ErrCode == PTT.ErrorCode.Success:
